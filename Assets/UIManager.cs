1cca92142a3606e55d7a1e6231f1f5770d02109d
ArekHanejko <81444898+ArekHanejko@users.noreply.github.com>
2025-05-29 15:38:16 +0200

    Update UIManager.cs

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -10,23 +10,7 @@ public class UIManager : MonoBehaviour
     public GameObject upgradeTowerBtn;
     public GameObject addTowerBtn;
 
-    void Start()
-    {
-        ResizePanel(towerBuildPanel);
-        ResizePanel(towerUpgradePanel);
-    }
-    private void ResizePanel(GameObject panel)
-    {
-        RectTransform rt = panel.GetComponent<RectTransform>();
-        float width = Screen.width * 0.25f;
-        float height = rt.sizeDelta.y; // zachowujemy obecną wysokość
-    
-        rt.anchorMin = new Vector2(0, 0.5f); // lewa środkowa kotwica
-        rt.anchorMax = new Vector2(0, 0.5f);
-        rt.pivot = new Vector2(0, 0.5f);     // obrót z lewej
-        rt.sizeDelta = new Vector2(width, height);
-        rt.anchoredPosition = new Vector2(0, 0); // przyklejony do lewej krawędzi
-    }
+
 
 
     private void Awake()
